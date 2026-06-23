800646c3d1e300ffae5e680f3b6d48697e79ea72
Naseef Nusky <naseefnusky09@gmail.com>
2026-06-23 19:21:12 +0530

    latest updates

--- src/components/common/GoogleAnalytics.jsx
+++ src/components/common/GoogleAnalytics.jsx
@@ -1,23 +1,26 @@
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 import { useLocation } from 'react-router-dom'
 import { initGoogleAnalytics, trackPageView } from '../../utils/analytics'
 
 export default function GoogleAnalytics() {
   const location = useLocation()
+  const lastTrackedPath = useRef(null)
 
   useEffect(() => {
-    let cancelled = false
     const path = `${location.pathname}${location.search}`
 
     initGoogleAnalytics().then(() => {
-      if (!cancelled) {
-        trackPageView(path)
-      }
-    })
+      if (lastTrackedPath.current === path) return
 
-    return () => {
-      cancelled = true
-    }
+      // index.html already sends the first page view on load
+      if (lastTrackedPath.current === null) {
+        lastTrackedPath.current = path
+        return
+      }
+
+      lastTrackedPath.current = path
+      trackPageView(path)
+    })
   }, [location.pathname, location.search])
 
   return null
